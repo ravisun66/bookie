01863c105c343150464e0a2a605742371c6a0f40
Ravi Shankar Jha <ravi.sun66@gmail.com>
2015-12-23 14:37:45 +0530

    aws dwnld simplfd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,38 +11,76 @@ class PagesController < ApplicationController
   def dashboard
   	@books = current_user.books
   end
+
   def perform
   	book = Book.find_by(:slug => params[:detail])
-  	zip_file_download_path = "#{Rails.root.join('tmp')}/new.zip"
-  	@zip_file_uncompressed_path = "#{Rails.root.join('tmp')}/books_folder"
-	 download = open(book.resource.url)
-	 IO.copy_stream(download, zip_file_download_path)
+  	zip_file_download_path = "#{Rails.root.join('tmp')}/#{params[:detail]}.zip"
+  	@zip_file_uncompressed_path = "#{Rails.root.join('tmp')}/#{params[:detail]}"
+    
+    if !(File.file?(zip_file_download_path))
+	     download = open(book.resource.url)
+	     IO.copy_stream(download, zip_file_download_path)
+    end
+
   	@pdf_files = []
     @video_files = []
-  	Zip::File.open(zip_file_download_path) do |zip_file|
-  		zip_file.each do |file|
-    		zip_file.extract(file, "#{@zip_file_uncompressed_path}/#{file.name}") unless File.exists?("#{@zip_file_uncompressed_path}/#{file.name}")
-			 if file.name.include? ".pdf"
-				  @pdf_files << file.name
-			 end
-       if file.name.include? ".mp4"
-        @video_files << file.name
-       end
-		  end
+  	
+    if File.directory?(@zip_file_uncompressed_path)
+      puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+      Dir.foreach(@zip_file_uncompressed_path) do |f_name|
+        if File.directory?("#{@zip_file_uncompressed_path}/#{f_name}") && !(f_name.include? ".")
+          
+          Dir.foreach("#{@zip_file_uncompressed_path}/#{f_name}") do |file_name|
+            if file_name.length > 2
+              
+                if file_name.include? ".pdf"
+                  @pdf_files << file_name
+                end
+                if file_name.include? ".mp4"
+                  @video_files << file_name
+                end
+
+          end
+          end
+
+        end
+        
+       
+      end
+    
+    else  
+
+        Zip::File.open(zip_file_download_path) do |zip_file|
+      		zip_file.each do |file|
+        		zip_file.extract(file, "#{@zip_file_uncompressed_path}/#{file.name}") unless File.exists?("#{@zip_file_uncompressed_path}/#{file.name}")
+    			 if file.name.include? ".pdf"
+    				  @pdf_files << file.name
+    			 end
+           if file.name.include? ".mp4"
+            @video_files << file.name
+           end
+    		  end
+        end
+
     end
+
+
+    #FileUtils.cp_r "#{@zip_file_uncompressed_path}", "#{Rails.public_path}/media/#{params[:detail]}"
+
   end
   
+
   def view_video
 
     @content_path = params[:content]
-    send_file ("#{@content_path}.mp4"), :type => 'video/mp4',:disposition => :inline, :stream => true 
+    #send_file ("#{@content_path}.mp4"), :type => 'video/mp4',:disposition => :inline, :stream => true 
 
   end
 
   def view_pdf
-
+    p ">>>>>>>>>>>here>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     @content_path = params[:content]
-    send_file ("#{@content_path}.pdf"), :type => 'application/pdf',:disposition => :inline, :stream => true 
+    #send_file ("#{@content_path}.pdf"), :type => 'application/pdf',:disposition => :inline, :stream => true 
 
   end
 
